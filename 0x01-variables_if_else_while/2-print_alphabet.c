86f8b428932d39e14163faadd2ce6814bac1f2bd
Armelle <armelledinde@yahoo.fr>
2022-03-11 20:36:40 +0100

    works2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c1;
+for (c1 = 'a'; c1 <= 'z'; c1++)
+{
+putchar (c1);
+}
+putchar('\n');
+return (0);
+}
