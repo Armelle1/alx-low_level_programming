eaf29c7be6f26e38b22f3a5783ca7672b09350c3
Armelle <armelledinde@yahoo.fr>
2022-03-11 22:06:39 +0100

    works9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,12 +7,16 @@
 int main(void)
 {
 int i;
-for (i = 0; i <= 9 ; i++)
+for (i = 0; i < 9 ; i++)
 {
-putchar (i);
-putchar (',');
-putchar (' ');
+	if (i != 9)
+	{
+		putchar (i + '0');
+		putchar (',');
+		putchar (' ');
+	}
 }
+putchar (i + '0');
 putchar('\n');
 return (0);
 }
