bb007c9f2a909c4fb600e41d1717cc114e0407aa
Armelle <armelledinde@yahoo.fr>
2022-03-17 09:27:50 +0100

    works 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isupper- cheks for uppercase character.
+ *@c: the parameter
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	return (1);
+return (0);
+}
