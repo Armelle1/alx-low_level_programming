dc7d615d40cba94bf3c40aeba809a2e341a36f47
Armelle <armelledinde@yahoo.fr>
2022-03-17 10:35:00 +0100

    works1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *_isdigit - check the digits
+ *@c: the character
+ * Return: Always 0.
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57) 
+		return (1);
+return (0);
+}
