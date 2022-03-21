e65ffdcaa48c96bc2dd089eb141d9fb3fba27e14
Armelle <armelledinde@yahoo.fr>
2022-03-21 15:46:26 +0100

    works1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - swap value.
+ * @a: the pointers.
+ * @b: the pointer.
+ *
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+       	*a = 42;
+	*b = 98;
+}
