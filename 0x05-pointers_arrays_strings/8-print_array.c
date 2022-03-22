280154ee94e8285c45a4e9504986fee604cff1b7
Armelle <armelledinde@yahoo.fr>
2022-03-22 23:03:07 +0100

    work 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints the second half of the string
+ *@a: first parameter;
+ *@n: second parameter
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n - 1 ; i++)
+	{
+		printf("%d, ", *(a + i));
+	}
+	printf("%d\n", *(a + i));
+}
