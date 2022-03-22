04493c5f1dcf3f2bc58a96a7bfe0bf95db848869
Armelle <armelledinde@yahoo.fr>
2022-03-22 22:31:42 +0100

    work 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * puts_half - prints the second half of the string
+ *@str: parameter;
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	char readedchar;
+	int numberofchar = 0;
+	int middle;
+	int i;
+
+	readedchar = *(str + numberofchar);
+	while (readedchar != '\0')
+	{
+		numberofchar++;
+		readedchar = *(str + numberofchar);
+	}
+	if ((numberofchar % 2) != 0)
+		middle = (numberofchar - 1) / 2;
+	else
+		middle = numberofchar / 2;
+	for (i = middle; i < numberofchar; i++)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
