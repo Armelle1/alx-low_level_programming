43a1b77e80817decd3de2b7a206572ae898252cd
Armelle <armelledinde@yahoo.fr>
2022-03-22 21:35:32 +0100

    works5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * rev_string- prints a string in reverse
+ *@s: parameter;
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{
+	char readedchar;
+	int numberofchar = 0;
+	int i;
+	char reverse[50];
+	int c = 0;
+
+	readedchar = *(s + numberofchar);
+	while (readedchar != '\0')
+	{
+		numberofchar++;
+		readedchar = *(s + numberofchar);
+	}
+	for (i = numberofchar - 1; i >= 0; i--)
+	{
+	reverse[c] = *(s + i);
+	c++;
+	}
+	for (c = 0; c < numberofchar; c++)
+	{
+		*(s + c) = reverse[c];
+	}
+}
