a1703d4de939ed0f1ba385f5ec0991047583bef2
Armelle <armelledinde@yahoo.fr>
2022-04-05 16:20:25 +0200

    works2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * str_concat- concatenate two strings
+ *@s1: the parameter
+ *@s2: the parameter
+ * Return: Always 0.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char readedchar1 = ' ';
+	char readedchar2 = ' ';
+	int indice2 = 0;
+	int indice1 = 0;
+	char *s3;
+	s3 = malloc((sizeofchar) *((indice1 + indice2) - 2);
+			while (readedchar1 != '\0')
+			{
+			readedchar1 = *(s1 + indice1);
+			indice1++;
+			}
+			while (readedchar2 != '\0')
+			{
+			readedchar2 = *(s2 + indice2);
+			indice2++;
+			}
+}
