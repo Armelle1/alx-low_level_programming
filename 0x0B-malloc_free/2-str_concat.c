7475920e654a83d5a0a046ad4403841b91857a00
Armelle <armelledinde@yahoo.fr>
2022-04-05 20:28:52 +0200

    work 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,8 +14,12 @@ char *str_concat(char *s1, char *s2)
 	int numberofchar = 0;
 	int numberofchar2 = 0;
 	char *s3;
-	int i;
-	int j;
+	int i, j;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	while (readedchar1 != '\0')
 	{
 		readedchar1 = *(s1 + numberofchar);
@@ -28,7 +32,7 @@ char *str_concat(char *s1, char *s2)
 		numberofchar2++;
 	}
 	numberofchar2--;
-	s3 = malloc((sizeof (char))*(numberofchar + numberofchar2));
+	s3 = malloc((sizeof(char)) * (numberofchar + numberofchar2 + 1));
 	if (s3 == NULL)
 	{
 		return (NULL);
@@ -39,5 +43,6 @@ char *str_concat(char *s1, char *s2)
 	}
 	for (j = 0; j < numberofchar2; i++, j++)
 		s3[i] = s2[j];
+	s3[i] = '\0';
 	return (s3);
 }
