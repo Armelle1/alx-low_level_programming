32954d4b76cd7c83613f67fe638790f1b2bb40bb
Armelle <armelledinde@yahoo.fr>
2022-04-05 14:38:38 +0200

    work 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,11 +12,11 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *a;
 
-	a = malloc(sizeof(char) * size);
 	if (size == 0)
 	{
 		return (NULL);
 	}
+	a = malloc(sizeof(char) * size);
 	for (i = 0; i < size ; i++)
 	{
 		a[i] = c;
