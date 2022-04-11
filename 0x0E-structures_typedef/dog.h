c136db2407be550c0211c4f9d783b9f1d53e9bb0
Armelle <armelledinde@yahoo.fr>
2022-04-11 08:30:06 +0200

    works0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef dog
+/**
+ * struct dog - information about dog's owner
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: for each owner, we have he name, age and owner
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+
+	float age;
+};
+#endif
