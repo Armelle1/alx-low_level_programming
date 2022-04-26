65d3a9bc4e96b3fd24e0200fb67562696b60b899
Armelle <armelledinde@yahoo.fr>
2022-04-26 17:42:48 +0200

    works5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - check the code
+ *@head: the parameter
+ * Return: Always 0.
+ */
+void free_listint2(listint_t **head)
+{
+	if (*head == NULL)
+		return;
+	free_listint2(&((*head)->next));
+	free(*head);
+	*head = NULL;
+}
