e8bb61ee92d3aa9f9316041121019ed0179b2155
Armelle <armelledinde@yahoo.fr>
2022-05-06 23:32:31 +0200

    work 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+/**
+ *create_file - creates a file
+ *@filename:the character
+ *@text_content:the parameter
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int z;
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_EXCL | O_RDWR, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		z = write(fd, text_content, strlen(text_content));
+		if (z == -1)
+			return (-1);
+
+	}
+	close(fd);
+	return (1);
+}
