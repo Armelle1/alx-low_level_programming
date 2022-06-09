c09517a0f73602a5a9fe0dfa75040391d8e31de4
Armelle <armelledinde@yahoo.fr>
2022-06-09 22:59:44 +0200

    work 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_dlistint - frees a dlistint_t list
+ * @head: pointer to list to be freed
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
+}
