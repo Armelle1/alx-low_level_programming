ee65b501f953abb078f1803e041d016597a61ddf
Armelle <armelledinde@yahoo.fr>
2022-06-10 00:26:46 +0200

    work 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -38,9 +38,16 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	if (index != idx)
 		return (NULL);
-	new->next = current;
-	new->prev = current->prev;
-	current->prev->next = new;
-	current->prev = new;
+	if (current->prev == NULL)
+	{
+		new->next = (*h)->next;
+		*h = new;
+	} else
+	{
+		new->next = current;
+		new->prev = current->prev;
+		current->prev->next = new;
+		current->prev = new;
+	}
 	return (new);
 }
